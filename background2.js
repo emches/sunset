6158872451f6b454df936829a158ec56a531ed5e
emches <emilychesler@gmail.com>
2018-04-06 17:39:31 -0400

    mvp

--- background2.js
+++ background2.js
@@ -41,26 +41,21 @@ function switchBackward(window){
   }
 }
 
-function handleTimer() {
+function handleTimer(tab1Time, tab2Time) {
   // console.log("element outside: ", document.getElementById("tab1Time"));
-  console.log("I WAS SUBMITTED")
+  console.log(`I was submitted with tab1Time of ${tab1Time}`);
+  console.log(`I was submitted with tab2Time of ${tab2Time}`);
+
   // const timer1 = parseInt(document.getElementById("tab1Time").value) * 1000;
   // const timer2 = parseInt(document.getElementById("tab2Time").value) * 1000;
     // console.log("element: ", document.getElementById("tab1Time"));
     // console.log("document: ", document);
-  let timer1 = 2000
-  let timer2 = 2000
 
-  console.log("timer 1: ", timer1);
-  console.log("timer 2: ", timer2);
   // $('#tab1Time').val("fgg");
   // $('#tab2Time').val("fgg");
 
 
   chrome.windows.getLastFocused( {populate: true}, function (window)  {
-
-
-      wait(2);
 
       for (var i = 0; i < window.tabs.length; i++) {
       // Finding the selected tab.
@@ -68,10 +63,9 @@ function handleTimer() {
           // Selecting the next tab.
           // chrome.tabs.update(window.tabs[i].id, {active: false});
           console.log(`updating ${i+1} to active`);
-          wait(2)
+          wait(tab1Time)
           chrome.tabs.update(window.tabs[i+1].id, {active: true, selected: true }, function(tab ){
             console.log("finished forward!", tab);
-            wait(5)
             chrome.windows.getLastFocused( {populate: true}, function (window)  {
               for (var i = 0; i < window.tabs.length; i++) {
               // Finding the selected tab.
@@ -79,11 +73,11 @@ function handleTimer() {
                 if (window.tabs[i].active) {
                   // Selecting the prev tab.
                   console.log(`going to tab ${i-1} `)
-                  wait(5)
+                  wait(tab2Time)
+
                   // chrome.tabs.update(window.tabs[i].id, {active: false});
                   chrome.tabs.update(window.tabs[i-1].id, {active: true}, function(){
                     console.log("finished backward!");
-                    wait(5)
                     return;
                   });
 
@@ -112,7 +106,7 @@ function handleTimer() {
 
 chrome.runtime.onMessage.addListener( function(request,sender,sendResponse) {
     if( request.greeting === "GetURL" ) {
-      handleTimer();
+      handleTimer(request.tab1Time, request.tab2Time);
       sendResponse("i worked!")
     }
 
--- content.js
+++ content.js
@@ -1,7 +1,13 @@
 $(document).ready(function () {
   console.log("READY")
   function submitTimer() {
-    chrome.runtime.sendMessage({greeting: "GetURL"},
+    let tab1Time = document.getElementById('tab1Time').value;
+    let tab2Time = document.getElementById('tab2Time').value;
+
+    console.log(`tab1Time is ${tab1Time}`);
+    console.log(`tab2Time is ${tab2Time}`);
+
+    chrome.runtime.sendMessage({greeting: "GetURL", tab1Time: tab1Time, tab2Time: tab2Time},
           function (response) {
               console.log("done", response)
           });
@@ -18,6 +24,7 @@ $(document).ready(function () {
   function afterDOMLoaded() {
     console.log("added listener")
     var timerSubmit = document.getElementById('timer-submit');
+    tab2Time
     console.log("timerSubmit", timerSubmit)
     timerSubmit.addEventListener('click', submitTimer);
   }
