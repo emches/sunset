f59731d63458794439db3686f27ce5835887f51a
emches <emilychesler@gmail.com>
2018-04-13 20:54:34 -0400

    track by video urls instead of index

--- background2.js
+++ background2.js
@@ -41,10 +41,37 @@ function switchBackward(window){
   }
 }
 
+function formatTime(timeString) {
+  if (timeString.indexOf(":") === 1) {
+      return `0${timeString}`;
+  }
+  return timeString;
+}
+
+function getTabID(tabs, url) {
+  for (var i = 0; i < tabs.length; i++) {
+    console.log(`comparing ${tabs[i].url} and ${url}`)
+    console.log(`comparing ${tabs[i].url.length} and ${url.length}`)
+    if (tabs[i].url === url) {
+      console.log(tabs[i]);
+      return tabs[i].id;
+    }
+  }
+
+  console.log("unable to locate that url in your tabs!");
+  return null;
+}
 function handleTimer(tab1Time, tab2Time) {
   // console.log("element outside: ", document.getElementById("tab1Time"));
   console.log(`I was submitted with tab1Time of ${tab1Time}`);
   console.log(`I was submitted with tab2Time of ${tab2Time}`);
+
+  tab1Time = formatTime(tab1Time);
+  tab2Time = formatTime(tab2Time);
+  console.log(`Formatted to ${tab1Time}`);
+  console.log(`Formatted to ${tab2Time}`);
+
+
 
   // const timer1 = parseInt(document.getElementById("tab1Time").value) * 1000;
   // const timer2 = parseInt(document.getElementById("tab2Time").value) * 1000;
@@ -56,44 +83,38 @@ function handleTimer(tab1Time, tab2Time) {
 
 
   chrome.windows.getLastFocused( {populate: true}, function (window)  {
-
-      for (var i = 0; i < window.tabs.length; i++) {
+      console.log(window.tabs)
+      const turkeyTabID = getTabID(window.tabs, "https://video.nest.com/live/CdolW540tc");
+      const corgiTab = getTabID(window.tabs, "https://www.youtube.com/watch?v=b9lb-T9FrAg");
+      console.log(`turkeyTabID: ${turkeyTabID}`);
       // Finding the selected tab.
-        if (window.tabs[i].active) {
           // Selecting the next tab.
           // chrome.tabs.update(window.tabs[i].id, {active: false});
-          console.log(`updating ${i+1} to active`);
-          wait(tab1Time)
-          chrome.tabs.update(window.tabs[i+1].id, {active: true, selected: true }, function(tab ){
-            console.log("finished forward!", tab);
-            chrome.windows.getLastFocused( {populate: true}, function (window)  {
-              for (var i = 0; i < window.tabs.length; i++) {
-              // Finding the selected tab.
-                console.log(`checking tab ${i}`)
-                if (window.tabs[i].active) {
-                  // Selecting the prev tab.
-                  console.log(`going to tab ${i-1} `)
-                  wait(tab2Time)
+      console.log(`updating ${turkeyTabID} to active`);
+      console.log(window)
+      wait(3)
+      chrome.tabs.update(turkeyTabID, {active: true, selected: true }, function(tab ){
+        console.log("finished forward!", tab);
+        chrome.windows.getLastFocused( {populate: true}, function (window)  {
+          // Finding the selected tab.
+              // Selecting the prev tab.
+            wait(3)
 
-                  // chrome.tabs.update(window.tabs[i].id, {active: false});
-                  chrome.tabs.update(window.tabs[i-1].id, {active: true}, function(){
-                    console.log("finished backward!");
-                    return;
-                  });
-
-                }
-              }
-
+            // chrome.tabs.update(window.tabs[i].id, {active: false});
+            chrome.tabs.update(corgiTab, {active: true}, function(){
+              console.log("finished backward!");
+              return;
             });
 
-          });
-        }
-      }
+
+        });
+
+      });
 
 
       const debug = new Date();
       while (Math.abs( new Date() - debug ) <= 8000){
-        console.log(`waiting ${timer2}ms`);
+        console.log(`waiting ${5}ms`);
       }
 
   });
